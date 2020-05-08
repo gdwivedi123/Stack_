a0fa3a4b09d43ff87452e082a660201e872ee864
gdwivedi123 <50792346+gdwivedi123@users.noreply.github.com>
2020-05-08 23:03:31 +0530

    Add files via upload
    Stack to perform insertion, deletion, and getMinimum methods in O(1) time.

---
+++ minstack.h
@@ -0,0 +1,42 @@
+class Min_Stack
+{
+    long int mini;
+    stack<long int> s;
+    public:
+        Min_Stack()
+        {
+            mini = INT_MAX;
+        }
+        void push(int x)
+        {
+            if(x >= mini)
+            {
+                s.push(x);
+            }
+            else
+            {
+                s.push(x - mini);
+                mini = x;
+            }
+        }
+        int pop()
+        {
+            if(isEmpty())
+				return -1;
+			int val = mini;
+            if(s.top() < mini)
+            {
+                mini = mini - s.top();
+            }
+            s.pop();
+            return val;
+        }
+		int getMin()
+		{
+			return mini;
+		}
+		bool isEmpty()
+		{
+			return s.empty();
+		}
+};
